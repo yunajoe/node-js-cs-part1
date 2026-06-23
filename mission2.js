24f886de2a39096fb9750e0263850961e143d580
yunajoe <yunajoe@gmail.com>
2026-06-23 20:00:54 +0900

    feat: mission2

---
+++ mission2.js
@@ -0,0 +1,4 @@
+import autoPilot, { GALAXY_COORDINATES } from "./navigation.js";
+
+console.log(autoPilot);
+console.log(GALAXY_COORDINATES);
---
+++ navigation.js
@@ -0,0 +1,4 @@
+export const GALAXY_COORDINATES = "Sector 7G";
+export default function autoPilot() {
+  console.log("자동 비행 모드를 시작합니다.");
+}
