558813b13ce93d1120c5640a6546c0d8d9c7e86e
yunajoe <yunajoe@gmail.com>
2026-06-23 19:21:00 +0900

    feat: default와 named 혼합 임포트

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import { getInfo as getAppleInfo } from "./apple.js";
 import { getInfo as getBananaInfo } from "./banana.js";
-import * as MathTools from "./math.js";
+
 import { log } from "./utils/logger.js";
 import { add } from "./utils/math.js";
+
+import * as MathTools from "./math.js";
+// 대표 기능인 connect는 중괄호 없이 밖으로, 일반 기능인 apiVersion은 중괄호 안에 적습니다.
+import connect, { apiVersion } from "./service.js";
 
 getAppleInfo();
 getBananaInfo();
@@ -10,3 +14,6 @@ console.log(MathTools.add(5, 3));
 console.log(MathTools.multiply(4, 2));
 
 log(add(10, 20));
+
+console.log(`현재 API 버전: ${apiVersion}`);
+connect();
---
+++ service.js
@@ -0,0 +1,7 @@
+// 일반 기능 (Named Export)
+export const apiVersion = "v2.0";
+
+// 대표 기능 (Default Export)
+export default function connect() {
+  console.log("서버에 연결되었습니다.");
+}
